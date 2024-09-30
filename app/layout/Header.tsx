d0db1da812b6a8a5aa144622e71a3c5e8d77fc1e
juliscrep <julietascrepnik@gmail.com>
2024-09-30 11:32:01 -0300

    set up auth

--- app/layout/Header.tsx
+++ app/layout/Header.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Link from 'next/link'
 
 type Props = {}
 
 const Header = (props: Props) => {
   return (
     <div className='border-b flex justify-between'>
-        <div className='text-4xl'>LOGO</div>
+        <Link href="/" className='text-4xl'>LOGO</Link>
         <button className='text-white bg-black px-4 py-4'>Sign In</button>
     </div>
   )
